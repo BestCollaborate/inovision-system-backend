8f6dfb9a03b7497d23adef66013443cc01611120
tech-master823 <carlsh0823@gmail.com>
2024-12-17 21:24:23 +0900

    util-chat

--- routes/lecture.route.js
+++ routes/lecture.route.js
@@ -4,12 +4,13 @@ const router = express.Router();
 
 import { authMiddleware } from '../middlewares/auth.middleware';
 
-const { storeLecture, getLectures, updateLecture, deleteLecture, getFreeLecture } = require('../controllers/lecture.controller');
+const { storeLecture, getLectures, updateLecture, deleteLecture, getFreeLecture, validateLecture } = require('../controllers/lecture.controller');
 
 router.post('/', authMiddleware, storeLecture);
 router.get('/', authMiddleware, getLectures);
 router.put('/:uid', authMiddleware, updateLecture);
 router.delete('/', authMiddleware, deleteLecture);    
 router.get('/free', authMiddleware, getFreeLecture);
+router.get('/validate/:uid/:lectureId/:role', authMiddleware, validateLecture);
 
 export default router;
