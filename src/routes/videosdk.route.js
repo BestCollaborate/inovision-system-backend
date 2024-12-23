85c6dff578d844ea4372f1bb7be9ff9abae38035
tech-master823 <carlsh0823@gmail.com>
2024-12-23 19:27:00 +0200

    sdk token generation

--- src/routes/videosdk.route.js
+++ src/routes/videosdk.route.js
@@ -1,21 +1,27 @@
 const router = require('express').Router();
 const jwt = require('jsonwebtoken');
 
-router.get('/video-sdk', (req, res) => {
-    const { permissions } = req.query;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
+router.get('/:role', (req, res) => {
+    const SECRET = process.env.VIDEOSDK_SECRET_KEY;
+
+    const { role } = req.params;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     const options = { 
         expiresIn: '120m', 
         algorithm: 'HS256' 
     };
+    const permissions = [];
+    console.log(req.params)
+    if (role === "student") permissions.push(`allow_join`);
+    if (role === "teacher") permissions.push(`allow_join`, `allow_mod`);
+    console.log(permissions);
     const payload = {
-        apikey: API_KEY,
-        permissions: [`allow_join`], // `ask_join` || `allow_mod` 
+        apikey: process.env.VIDEOSDK_API_KEY,
+        version: 2,
+        permissions
     };
     
     const token = jwt.sign(payload, SECRET, options);
-    console.log(token);
-    
-       
+    res.json({ apiToken: token });       
 });
 
 module.exports = router;
